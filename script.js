b5fcda1a5263644118f59f5acf91d6e8f0fee112
Shattered-Star <95058040+Shattered-Star@users.noreply.github.com>
2022-08-24 12:00:31 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+<script>
+function option1(){
+	document.getElementById("bot-avatar").src = "assets/bot-1.webp"
+	document.getElementById("name1").innerHTML = "Question> "
+	document.getElementById("message1").innerHTML = "Who is Abdullah?"
+	document.getElementById("name2").innerHTML = "Beepo> "
+	document.getElementById("message2").innerHTML = "His full name is Abdullah Gilacio Bakhsh. He currently lives in Makati City."
+}
+
+function option2(){
+	document.getElementById("bot-avatar").src = "assets/bot-1.webp"
+	document.getElementById("name1").innerHTML = "Question> "
+	document.getElementById("message1").innerHTML = "How old is he?"
+	document.getElementById("name2").innerHTML = "Beepo> "
+	document.getElementById("message2").innerHTML = "He is 14 years old currently. He was born in May 5, 2008."
+}
+
+function option3(){
+	document.getElementById("bot-avatar").src = "assets/bot-2.webp"
+	document.getElementById("name1").innerHTML = "Question> "
+	document.getElementById("message1").innerHTML = "What are his hobbies?"
+	document.getElementById("name2").innerHTML = "Beepo> "
+	document.getElementById("message2").innerHTML = "He loves reading various novels, especially those belonging to the sci-fi and fantasy genre with a special interest on xianxia. He also has a special appreciation for pixel games and roguelikes such as Dead Cells, Hades, and Minecraft with hours invested in the three mentioned. He also likes coding for fun, with this page being an example."
+}
+
+function option4(){
+	document.getElementById("bot-avatar").src = "assets/bot-2.webp"
+	document.getElementById("name1").innerHTML = "Question> "
+	document.getElementById("message1").innerHTML = "What are his dreams?"
+	document.getElementById("name2").innerHTML = "Beepo> "
+	document.getElementById("message2").innerHTML = "He feels lost right now as to what to become and which path to choose. The only thing that is certain is that he has a very special interest for computers."
+}
+
+function option5(){
+	document.getElementById("bot-avatar").src = "assets/bot-3.webp"
+	document.getElementById("name1").innerHTML = "Question> "
+	document.getElementById("message1").innerHTML = "What is his favorite music with no lyrics?"
+	document.getElementById("name2").innerHTML = "Beepo> "
+	document.getElementById("message2").innerHTML = "Sweden by C418."
+}
+
+function option6(){
+	document.getElementById("bot-avatar").src = "assets/bot-4.webp"
+	document.getElementById("name1").innerHTML = "Question> "
+	document.getElementById("message1").innerHTML = "What is his online alias?"
+	document.getElementById("name2").innerHTML = "Beepo> "
+	document.getElementById("message2").innerHTML = "He often goes by Shattered Star in various platforms"
+}
